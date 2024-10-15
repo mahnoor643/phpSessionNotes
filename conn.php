a522899e71272480e32c3f487c23738ed2535e05
mahnoor643 <mahnoort643@gmail.com>
2024-10-15 21:44:04 +0500

    database connection

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+  $conn=mysqli_connect("localhost","root","","todoList");
+//   if($conn){
+//     echo "<script>alert('Database connected');</script>" ;
+//   }else{
+//     die(mysqli_error());
+//   }
+?>
